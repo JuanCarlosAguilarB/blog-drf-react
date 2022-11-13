66bd962b3b06a0bfa73c512e3336a4a5811cc364
JuanCarlosAguilarB <abjuancarlos.12@gmail.com>
2022-11-13 14:16:51 -0500

    se agrega actions para blog

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-    path('api/blog/', admin.site.urls),
-    path('admin/', include("apps.blog.urls")),
-    path('admin/', include("apps.category.urls")),
+    path('api/blog/', include('apps.blog.urls')),
+    path('api/category/', include('apps.category.urls')),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 urlpatterns += [re_path(r'^.*',
--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Home from 'containers/pages/Home';
+import Error404 from 'containers/errors/Error404';
+
+import { Provider } from 'react-redux';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { store } from 'app/store';
+ 
 
 function App() {
   return (
+    <Provider store={store}>
+      <Router>
+        <Routes> 
+          <Route path='*' element = {<Error404/>} />
+          <Route path='?' element = {<Home/>} />
+        </Routes>
     <div className="App">
-         <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
@@ -55,6 +65,8 @@ function App() {
         </span>
       </header>
     </div>
+    </Router>
+  </Provider>
   );
 }
 

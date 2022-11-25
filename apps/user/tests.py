7705e2096e5e4099696c984a6324fa81d8ea180c
Juan Carlos Aguilar B <abjuancarlos.12@gmail.com>
2022-11-25 15:34:33 -0500

    test for create user and superuser

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,14 +1,54 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
 class UsersTests(TestCase):
     """Test for user model"""
     
     def setUp(self):
         """initial config for all test"""
-        self.payload = {"email":'normal@user.com', "password":'F0001@'}
+        self.email = 'normal@user.com'
+        self.payload = {"email":self.email, "password":'12345F678@'}
+        
+
+    def test_constraints_of_password_of_user(self):
+        """test for not create an user whitout constraints of password"""
+        
+        User = get_user_model()
+        
+        # failded because it does not meet the minimun lenght 
+        with self.assertRaises(ValidationError):
+            user = User.objects.create_user(self.email, "F1@")
+        
+        # failded because it does not have capitalize letter
+        with self.assertRaises(ValidationError):
+            user = User.objects.create_user(self.email, "12345678@")
+        
+        # failded because it does not have special character
+        with self.assertRaises(ValidationError):
+            user = User.objects.create_user(self.email, "12F345678")
+ 
+    def test_constraints_of_password_of_superuser(self):
+        """test for not create a superuser whitout constraints of password"""
+        
+        User = get_user_model()
+        
+        # failded because it does not meet the minimun lenght 
+        with self.assertRaises(ValidationError):
+            user = User.objects.create_superuser(self.email, "F1@")
+        
+        # failded because it does not have capitalize letter
+        with self.assertRaises(ValidationError):
+            user = User.objects.create_superuser(self.email, "12345678@")
+        
+        # failded because it does not have special character
+        with self.assertRaises(ValidationError):
+            user = User.objects.create_superuser(self.email, "12F345678")           
+            
 
     def test_create_user_sussecsfull(self):
+        """test for create user whit email"""
+        
         User = get_user_model()
         user = User.objects.create_user(**self.payload)
         
@@ -19,32 +59,15 @@ class UsersTests(TestCase):
         
         self.assertTrue(user.check_password(self.payload["password"]), self.payload["password"])
         
-        # try:
-        #     # username is None for the AbstractUser option
-        #     # username does not exist for the AbstractBaseUser option
-        #     self.assertIsNone(user.username)
-        # except AttributeError:
-        #     pass
-        # with self.assertRaises(TypeError):
-        #     User.objects.create_user()
-        # with self.assertRaises(TypeError):
-        #     User.objects.create_user(email='')
-        # with self.assertRaises(ValueError):
-        #     User.objects.create_user(email='', password="foo")
-
-    # def test_create_superuser(self):
-    #     User = get_user_model()
-    #     admin_user = User.objects.create_superuser(email='super@user.com', password='foo', username="admin")
-    #     self.assertEqual(admin_user.email, 'super@user.com')
-    #     self.assertTrue(admin_user.is_active)
-    #     self.assertTrue(admin_user.is_staff)
-    #     self.assertTrue(admin_user.is_superuser)
-    #     try:
-    #         # username is None for the AbstractUser option
-    #         # username does not exist for the AbstractBaseUser option
-    #         self.assertIsNone(admin_user.username)
-    #     except AttributeError:
-    #         pass
-    #     with self.assertRaises(ValueError):
-    #         User.objects.create_superuser(
-    #             email='super@user.com', password='foo', is_superuser=False)
+    def test_create_superuser_sussecsfull(self):
+        """test for create superuser whit email"""
+        
+        User = get_user_model()
+        user = User.objects.create_superuser(**self.payload)
+        
+        self.assertEqual(user.email, self.payload["email"])
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+        
+        self.assertTrue(user.check_password(self.payload["password"]), self.payload["password"])

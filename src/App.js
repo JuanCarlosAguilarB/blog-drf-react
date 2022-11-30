61516f2a2b057eeef3db53a4ae8e65ee92d972c3
Juan Carlos Aguilar B <abjuancarlos.12@gmail.com>
2022-11-29 22:50:32 -0500

    add view of login (front)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import './App.css';
-import Home from 'containers/pages/Home';
-import Error404 from 'containers/errors/Error404';
+import "./App.css";
+import Home from "containers/pages/Home";
+import Error404 from "containers/errors/Error404";
 
-import { Provider } from 'react-redux';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { store } from 'app/store';
-import Blog from 'containers/pages/blog/Blog';
-import BlogPost from 'containers/pages/blog/BlogPost';
-import BlogCategory from 'containers/pages/blog/category/BlogCategory';
-import Search from 'containers/pages/Search';
- 
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { store } from "app/store";
+import Blog from "containers/pages/blog/Blog";
+import BlogPost from "containers/pages/blog/BlogPost";
+import BlogCategory from "containers/pages/blog/category/BlogCategory";
+import Search from "containers/pages/Search";
+import Login from "containers/pages/Login";
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Routes> 
+        <Routes>
           {/* Error Display */}
-          <Route path="*" element={<Error404/>}/>
+          <Route path="*" element={<Error404 />} />
 
           {/* Home Display */}
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
 
-          <Route path="/blog" element={<Blog/>}/>
-          <Route path="/blog/post/:slug" element={<BlogPost/>}/>
-          <Route path="/blog/categories/:category_id" element={<BlogCategory/>}/>
-
-          <Route path="/search/:term" element={<Search/>}/>
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/post/:slug" element={<BlogPost />} />
+          <Route path="/blog/post/:slug" element={<BlogPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/search/:term" element={<Search />} />
         </Routes>
-    </Router>
-  </Provider>
+      </Router>
+    </Provider>
   );
 }
 
--- src/containers/pages/Login.jsx
+++ src/containers/pages/Login.jsx
@@ -1,32 +1,78 @@
-import React from 'react'
-import loginImg from "assets/login.jpg"
+import React, { useState } from "react";
+import "./login.css";
+import { Link } from "react-router-dom";
+import loginimg from "assets/login.jpg";
+import { login, logout } from "features/counter/userSlice";
+import { useDispatch } from "react-redux";
 
-export default function Login() {
+
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(
+      login({
+        email: email,
+        password: password,
+        loggedIn: true,
+      })
+    );
+
+    setEmail("");
+    setPassword("");
+  };
+
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-        <div className='hidden sm:block'>
-            <img className='w-full h-full object-cover' src={loginImg} alt="" />
-        </div>
+    <div className="login">
+      <div>
+        <img src={loginimg} />
+      </div>
 
-        <div className='bg-gray-800 flex flex-col justify-center'>
-            <form className='max-w-[400px] w-full mx-auto rounded-lg bg-gray-900 p-8 px-8'>
-                <h2 className='text-4xl dark:text-white font-bold text-center'>SIGN IN</h2>
-                <div className='flex flex-col text-gray-400 py-2'>
-                    <label>Username</label>
-                    <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" />
-                </div>
-                <div className='flex flex-col text-gray-400 py-2'>
-                    <label>Password</label>
-                    <input className='p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="password" />
-                </div>
-                <div className='flex justify-between text-gray-400 py-2'>
-                    <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
-                    <p>Forgot Password</p>
-                </div>
-                <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>SIGNIN</button>
-                
-            </form>
-        </div>
+      <div className="main-container">
+        <p>Welcome!</p>
+
+
+        <form className="login__form" onSubmit={(e) => handleSubmit(e)}>
+          <input className="input-login"
+            type="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input className="input-login"
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div  className="container-login-options">
+            <div className="container-login-options-checkbox">
+               <input type="checkbox" value="remember" id="checkbox-remember"/>
+                <label htmlFor="checkbox-remember">Remember me </label>
+            </div>
+
+
+            <Link className="container-login-options-Link " to="" > Forgot password</Link>
+          </div>
+          
+          <button type="submit" className="submit__btn">
+            Submit
+          </button>
+
+
+        </form>
+
+
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Login;

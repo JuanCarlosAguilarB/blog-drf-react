d76da44f184b7237858436efecb4d4a3b9403b93
Juan Carlos A <abjuancarlos.12@gmail.com>
2024-05-10 22:27:56 -0500

    chore: update to vite

--- apps/commons.py
+++ apps/commons.py
@@ -2,10 +2,10 @@
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 
-#Django Rest Framework
+# Django Rest Framework
 from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAdminUser
-from rest_framework import status,viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 
@@ -13,9 +13,10 @@ class ListModelMixin:
     """
     List a queryset whit pagination for GenericViewSet.
     """
+
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset() 
-        
+        queryset = self.get_queryset()
+
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
